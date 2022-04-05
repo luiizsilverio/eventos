566800bc21ce4b218acf3ded3221ebf142a0a900
luiizsilverio <luiiz.silverio@gmail.com>
2022-04-05 19:53:58 -0300

    login por email

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,17 @@
 import firebase from "firebase"
-// import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 if (!firebase.apps.length) {
-  initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 export default firebase
--- src/view/login/index.jsx
+++ src/view/login/index.jsx
@@ -1,26 +1,74 @@
+import { useState } from 'react'
 import './login.css'
+import firebase from '../../config/firebase'
+import 'firebase/auth'
 
 export default function Login() {
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [logado, setLogado] = useState("")
+
+
+  function logar() {
+    if (!email || !senha) {
+      return
+    }
+
+    const auth = firebase.auth()
+
+    auth.signInWithEmailAndPassword(email, senha)
+      .then(resp => {
+        setLogado("sim")
+        console.log(resp)
+      })
+      .catch(erro => {
+        setLogado("não")
+      })
+  }
+
   return (
     <div className="login-content d-flex align-items-center">
       <form className="form-signin mx-auto">
         <h1 className="h3 mb-3 fw-normal text-white fw-bold">Log In</h1>
 
         <div className="form-floating my-2">
-          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+          <input type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label htmlFor="floatingInput">E-mail</label>
         </div>
         <div className="form-floating my-2">
-          <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+          <input type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+            required
+            onChange={(e) => setSenha(e.target.value)}
+          />
           <label htmlFor="floatingPassword">Senha</label>
         </div>
 
-        <button className="w-100 btn btn-lg btn-primary btn-login" type="submit">Entrar</button>
+        <button
+          className="w-100 btn btn-lg btn-primary btn-login"
+          type="button"
+          onClick={logar}
+        >
+          Entrar
+        </button>
 
         <div className="msg-login text-center span my-4">
-          <span className="my-span">Você está conectado! &#128526;</span>
-          <br />
-          <span className="my-span">Ops! Verifique se a senha ou usuário estão corretos! &#128577;</span>
+          {
+            logado === 'sim' &&
+              <span className="my-span">Você está conectado! &#128526;</span>
+          }
+          {
+            logado === 'não' &&
+              <span className="my-span">Ops! Verifique se a senha ou usuário estão corretos! &#128577;</span>
+          }
         </div>
 
         <div className="opcoes-login m-4">
