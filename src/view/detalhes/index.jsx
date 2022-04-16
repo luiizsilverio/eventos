93181d7ee5b1a2abebe58282d5b3b3b29a3313bb
luiizsilverio <luiiz.silverio@gmail.com>
2022-04-16 11:31:35 -0300

    Detalhes OK

--- src/view/detalhes/index.jsx
+++ src/view/detalhes/index.jsx
@@ -1,50 +1,78 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+
+import 'firebase/storage'
+import 'firebase/firestore'
 import firebase from '../../config/firebase'
 
 import './detalhes.css'
 import Navbar from '../../components/navbar'
 
-export default function Detalhes() {
+export default function Detalhes(props) {
+  const [evento, setEvento] = useState({})
+  const [urlImg, setUrlImg] = useState("")
+  const { id } = useParams()
+
+  const storage = firebase.storage()
+  const db = firebase.firestore()
+  const usuario = useSelector(state => state.email)
+
+  useEffect(() => {
+    db.collection('eventos').doc(id).get()
+      .then(response => {
+        const dados = response.data()
+        setEvento(dados)
+
+        storage.ref(`imagens/${dados.foto}`).getDownloadURL()
+          .then(url => setUrlImg(url))
+      })
+  }, [])
+
   return (
     <>
       <Navbar />
       <div className="container-fluid">
         <div className="row">
-          <img src="https://via.placeholder.com/150x100"
-            className='img-banner' alt="Foto"
-          />
+          <img src={urlImg} className='img-banner' alt="Foto" />
+          <div className="col-12 eye">
+            <i className='fas fa-eye' /><span>{evento.visualizacoes}</span>
+          </div>
         </div>
 
         <div className="row mt-5 d-flex justify-content-around">
           <div className="col-md-3 col-sm-12 box-info p-3 my-2">
             <i className='fa-solid fa-ticket fa-2x' />
             <h4><strong>Tipo</strong></h4>
-            <span className='mt-3'>Festa</span>
+            <span className='mt-3'>{evento.tipo}</span>
           </div>
 
           <div className="col-md-3 col-sm-12 box-info p-3 my-2">
             <i className='fa-solid fa-calendar-days fa-2x' />
             <h4><strong>Data</strong></h4>
-            <span className='mt-3'>15/04/2022</span>
+            <span className='mt-3'>{evento.data}</span>
           </div>
 
           <div className="col-md-3 col-sm-12 box-info p-3 my-2">
             <i className='fa-solid fa-clock fa-2x' />
             <h4><strong>Hora</strong></h4>
-            <span className='mt-3'>20:57</span>
+            <span className='mt-3'>{evento.hora}</span>
           </div>
         </div>
 
         <div className="row box-detalhes mt-5">
-          <h4 className='text-center'><strong>Detalhes do Evento</strong></h4>
-          <p className="p-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <h3 className='text-center'><strong>{evento.titulo}</strong></h3>
+          <p className="p-3">{evento.detalhes}</p>
         </div>
 
-        <Link to="/" className='btn-editar'>
-          <i className='fa-solid fa-square-pen fa-3x' />
-        </Link>
+        {
+          usuario === evento.usuario && (
+            <Link to="/" className='btn-editar'>
+              <i className='fa-solid fa-square-pen fa-3x' />
+            </Link>
+          )
+        }
       </div>
     </>
   )
